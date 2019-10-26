cb1b878d82a1b9d0706f84609eb2931531ebf065
David Poirier-Quinot <davipoir@gmail.com>
2019-10-27 01:21:27 +0200

    beautify ui and disable it when auralization running

--- ui.py
+++ ui.py
@@ -29,37 +29,38 @@ class EvertimsToolBar(EvertimsUIBase, Panel):
 
         # Network configuration
         box = layout.box()
+        box.enabled = not evertims.enable_auralization
         box.label("Network", icon='URL')
 
         rowsub = box.row(align=True)
-        rowsub.label("Evertims client IP adress & port:")
-        rowsub = box.row(align=True)
         split = rowsub.split(percentage=0.6)
         colsub = split.column()
-        colsub.prop(evertims, "ip_remote", text="")
+        colsub.prop(evertims, "ip_remote", text="ip remote")
         colsub = split.column()
-        colsub.prop(evertims, "port_write", text="port")
+        colsub.prop(evertims, "port_write", text="port write")
 
         rowsub = box.row(align=True)
         split = rowsub.split(percentage=0.6)
         colsub = split.column()
-        colsub.prop(evertims, "ip_local", text="")
+        colsub.prop(evertims, "ip_local", text="ip local")
         colsub = split.column()
-        colsub.prop(evertims, "port_read", text="port")
+        colsub.prop(evertims, "port_read", text="port read")
 
-        rowsub = box.row(align=True)
-        if evertims.is_client_connected:
-            rowsub.label("client detected")
-        else:
-            rowsub.label("client not detected")
+        # rowsub = box.row(align=True)
+        # if evertims.is_client_connected:
+        #     rowsub.label("client detected")
+        # else:
+        #     rowsub.label("client not detected")
 
         # Engine configuration
         box = layout.box()
-        box.label("Engine configuration", icon='LAMP_AREA')
+        box.enabled = not evertims.enable_auralization
+        box.label("Engine configuration", icon='PREFERENCES')
+
         rowsub = box.row(align=True)
-        rowsub.prop(evertims, "engine_type", expand=True)
+        rowsub.prop(evertims, "engine_type", expand=False)
         rowsub = box.row(align=True)
-        rowsub.prop(evertims, "ism_max_order", text="max order")
+        rowsub.prop(evertims, "ism_max_order", text="ism order max")
         rowsub = box.row(align=True)
         rowsub.prop(evertims, "sound_velocity", text="sound velocity (m/s)")
         rowsub = box.row(align=True)
@@ -73,46 +74,55 @@ class EvertimsToolBar(EvertimsUIBase, Panel):
         colsub = split.column()
         colsub.prop(evertims, "update_thresh_rot", text="rot (deg)")
         rowsub = box.row(align=True)
-        rowsub.prop(addon_prefs, "material_file_path", text="mat file")
+        rowsub.prop(addon_prefs, "material_file_path", text="material file")
         rowsub = box.row(align=True)
-        rowsub.operator("evert.import", text="refresh materials", icon="NONE").arg ='materials'
-
-        # Drawer configuration
-        rowsub = box.row(align=True)
-        rowsub.prop(evertims, "draw_rays", text="draw rays in 3D view")
-        rowsub = box.row(align=True)
-        rowsub.prop(evertims, "draw_order_max", text="max order")
-
-        rowsub = box.row(align=True)
-        rowsub.prop(evertims, "debug_logs", text="print logs to console")
-        rowsub = box.row(align=True)
-        if not evertims.enable_auralization:
-            rowsub.operator("evert.run", text="START", icon="RADIOBUT_OFF").arg ='start'
-        else:
-            rowsub.operator("evert.run", text="STOP", icon="REC").arg ='stop'
-
-        # Exporter
-        # rowsub = box.row(align=True)
-        # rowsub.operator("evert.export", text="Export", icon="TEXT").arg =''
-        # rowsub = box.row(align=True)
-        # rowsub.operator("evert.crystalize", text="Crystalize visible rays", icon="HAIR").arg =''
-
+        rowsub.operator("evert.import", text="Refresh Materials", icon="FILE_REFRESH").arg ='materials'
 
         # Import elements
-        box = layout.box()
-        box.label("Import elements", icon='GROUP')
+        # box = layout.box()
+        # box.label("Import elements", icon='GROUP')
         # rowsub = box.row(align=True)
         # rowsub.operator("evert.import_template", text="Template scene", icon='MESH_CUBE').arg = 'scene'
 
         # Define KX_GameObjects as EVERTims elements
         box = layout.box()
-        box.label("Define as Evertims element", icon='PINNED')        
+        box.enabled = not evertims.enable_auralization
+        box.label("Define as Evertims element", icon='GROUP')
+
         col = box.column(align=True)
         col.prop_search(evertims, "room_object", bpy.data, "objects")
         col = box.column(align=True)
         col.prop_search(evertims, "listener_object", bpy.data, "objects")
         col = box.column(align=True)
         col.prop_search(evertims, "source_object", bpy.data, "objects")
+
+        # Auralization run
+        box = layout.box()
+        box.label("Run auralization", icon='LAMP_AREA')
+
+        rowsub = box.row(align=True)
+        rowsub.enabled = not evertims.enable_auralization
+        split = rowsub.split(percentage=0.5)
+        colsub = split.column()
+        colsub.prop(evertims, "draw_rays", text="draw paths")
+        colsub = split.column()
+        colsub.prop(evertims, "draw_order_max", text="draw order max")
+        colsub.enabled = not evertims.enable_auralization
+
+        rowsub = box.row(align=True)
+        rowsub.prop(evertims, "debug_logs", text="print logs to console")
+        rowsub.enabled = not evertims.enable_auralization
+        rowsub = box.row(align=True)
+        if not evertims.enable_auralization:
+            rowsub.operator("evert.run", text="Start", icon="RADIOBUT_OFF").arg ='start'
+        else:
+            rowsub.operator("evert.run", text="Stop", icon="REC").arg ='stop'
+
+        # Exporter
+        # rowsub = box.row(align=True)
+        # rowsub.operator("evert.export", text="Export", icon="TEXT").arg =''
+        # rowsub = box.row(align=True)
+        # rowsub.operator("evert.crystalize", text="Crystalize visible rays", icon="HAIR").arg =''
 
 
 # ############################################################
